d60feadb8572123c5a13ccb1de68309b0c42ff15
Dmitry Tretyakov <tinelix@mail.ru>
2026-04-22 15:50:53 +0700

    Added another question regarding the use of debugging symbols

--- make.bat
+++ make.bat
@@ -1,6 +1,15 @@
 @echo off
 echo.
 if NOT DEFINED CROCON_CC_TARGET (
+	echo You run a script designed to build the Crocon framework with demo application 
+	echo samples on Windows.
+	echo.
+	echo Before starting the build, you need to configure the build.
+	echo Currently, two target versions of Microsoft Visual C++ are supported:
+	echo.
+	echo [vc2] Microsoft Visual C++  2.0 - 2005
+	echo [vc8] Microsoft Visual C++ 2005 - 2026
+	echo.
 	set /P "CROCON_CC_TARGET=Choose your MSVC target version [vc2, vc8]: "
 )
 
@@ -26,7 +35,7 @@ nmake -nologo -f %CROCON_CC_TARGET%.mak
 cd ..\..\..
 
 echo.
-echo [2/2] Building demos...
+echo [2/2] Building Crocon Demos...
 
 cd demos\barebns1\proj\msvc
 nmake -nologo -f %CROCON_CC_TARGET%.mak CROCON_ROOT=..\..\..\..
