596fa24d4e8378bd6b0c4948095bc7ab063c831d
Dmitry Tretyakov <tinelix@mail.ru>
2026-04-22 15:38:00 +0700

    Added another question regarding the use of debugging symbols

--- make.bat
+++ make.bat
@@ -1,56 +1,43 @@
-PROJECT 			= barebns1
+@echo off
+echo.
+if NOT DEFINED CROCON_CC_TARGET (
+	set /P "CROCON_CC_TARGET=Choose your MSVC target version [vc2, vc8]: "
+)
 
-BAREBNS1_ROOT	= ..\..
-
-INC_DIR				= $(BAREBNS1_ROOT)\include
-SRC_DIR 			= $(BAREBNS1_ROOT)\src
-RES_DIR				= $(BAREBNS1_ROOT)\res
-
-BIN_DIR 			= $(CROCON_ROOT)\out\demos\$(PROJECT)\bin
-OBJ_DIR 			= $(CROCON_ROOT)\out\demos\$(PROJECT)\obj
-
-C_FLAGS				= -MT -W3 -Zi -BHsc -Od
-
-!ifdef DEBUG
-	C_FLAGS 	= -Z7 -DDEBUG
-	LD_FLAGS	= -debug -pdb:none
-!endif
+if NOT DEFINED CROCON_CC_TARGET (set CROCON_CC_TARGET=vc2)
 
 
-LD_FLAGS 			= -subsystem:console
-LD_LIBS				= user32.lib $(CROCON_ROOT)\out\library\bin\crocon.lib
+if NOT DEFINED CROCON_DEBUG (
+	set /P "CROCON_DEBUG=Include debug symbols before building? [y, n]: "
+)
 
-CC_FLAGS			= $(C_FLAGS) -I$(INC_DIR) -I$(CROCON_ROOT)\library\include \
-				        -DWIN32 -DWINDOWS
+if DEFINED CROCON_DEBUG IF "%CROCON_DEBUG%"=="y" (
+	set DEBUG=1
+) else (
+	set DEBUG=
+)
 
-CC_FLAGS_DLL	= $(C_FLAGS) -I$(INC_DIR)
+echo.
+echo [1/2] Building Crocon TUI framework from source...
 
-CC				    = cl -nologo
-LINKER				= link.exe -nologo
+cd ..\library\proj\msvc
+nmake -nologo -f %CROCON_CC_TARGET%.mak
 
-OUT_EXE				= $(BIN_DIR)\barebns1.exe
+cd ..\..\..
 
-OBJECTS 			= $(OBJ_DIR)\main.obj
+echo.
+echo [2/2] Building demos...
 
-all: prepare $(OUT_EXE)
+cd demos\barebns1\proj\msvc
+nmake -nologo -f %CROCON_CC_TARGET%.mak CROCON_ROOT=..\..\..\..
+echo.
+echo Done!
 
-$(OUT_EXE): $(OBJECTS)
-	$(LINKER) $(LD_FLAGS) $(LD_LIBS) -out:$@ $**
+cd ..\..\..\..\build
 
-{$(SRC_DIR)}.c{$(OBJ_DIR)}.obj:
-	$(CC) $(CC_FLAGS) -c $< -Fo$@
+set DEBUG=
+set CROCON_DEBUG=
 
-{$(SRC_DIR)\os\win32}.c{$(OBJ_DIR)}.obj:
-	$(CC) $(CC_FLAGS) -c $< -Fo$@
+echo.
 
-prepare:
-	@if not exist $(CROCON_ROOT)\out mkdir $(CROCON_ROOT)\out
-	@if not exist $(CROCON_ROOT)\out\demos mkdir $(CROCON_ROOT)\out\demos
-	@if not exist $(CROCON_ROOT)\out\demos\$(PROJECT) mkdir $(CROCON_ROOT)\out\demos\$(PROJECT)
-	@if not exist $(OBJ_DIR) mkdir $(OBJ_DIR)
-	@if not exist $(BIN_DIR) mkdir $(BIN_DIR)
-
-clean:
-	-del $(OBJ_DIR)\*.obj
-	-del $(BIN_DIR)\*.exe
-	-del $(BIN_DIR)\*.exp
+pause
